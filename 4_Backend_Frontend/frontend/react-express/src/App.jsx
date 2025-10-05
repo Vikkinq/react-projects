ab5898dbc189b89d11d9cab4154b25f10f008fba
Symon <fritzsymon23@gmail.com>
2025-10-05 19:24:36 +0800

    Another Practice

---
+++ 4_Backend_Frontend/frontend/react-express/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((serverData) => {
+        setData(serverData);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Express - React Connection</h1>
+      <hr />
+      {typeof data.data === "undefined" ? <p>Loading...</p> : data.data.map((d, i) => <h5 key={i}>{d}</h5>)}
+    </>
+  );
+}
+
+export default App;
