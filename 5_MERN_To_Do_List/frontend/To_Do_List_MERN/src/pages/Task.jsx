2bef13250a211b428eabb55113b140fbbc801d02
Symon <fritzsymon23@gmail.com>
2025-10-07 11:52:29 +0800

    Fixing

--- 5_MERN_To_Do_List/frontend/To_Do_List_MERN/src/pages/Task.jsx
+++ 5_MERN_To_Do_List/frontend/To_Do_List_MERN/src/pages/Task.jsx
@@ -7,11 +7,20 @@ export default function Task() {
   const [task, setTask] = useState([]);
 
   useEffect(() => {
-    fetch("/api/tasks")
-      .then((res) => res.json())
-      .then((taskData) => {
-        setTask(taskData);
-      });
+    try {
+      const fetchTasks = async () => {
+        try {
+          const res = await fetch("/api/tasks");
+          const taskData = await res.json();
+          setTask(taskData);
+        } catch (err) {
+          console.error("Failed to fetch tasks:", err);
+        }
+      };
+      fetchTasks();
+    } catch (err) {
+      console.log("Error has Occured: ", err);
+    }
   }, []);
 
   const handleDelete = async (id) => {
